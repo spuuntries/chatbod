30b05fa87dd3ae3ff4037b423cf379ffce00f808
spuun <kek@spuun.art>
2023-04-02 23:10:48 +0700

    Intents are sht I tell you

--- simpleindex.js
+++ simpleindex.js
@@ -1,15 +1,24 @@
 require("dotenv").config();
 const procenv = process.env,
   Discord = require("discord.js"),
-  client = new Discord.Client({ intents: ["MessageContent"] }),
+  client = new Discord.Client({
+    intents: ["Guilds", "GuildMessages", "MessageContent"],
+  }),
   { runPrompt } = require("./llmUtils"),
   logger = (m) => console.log(`[${new Date()}] ${m}`);
 
 client.on("messageCreate", async (message) => {
-  if (!message.content) return;
+  if (!message.content || message.author.id == client.user.id) return;
   const history = Array.from(
       (await message.channel.messages.fetch({ limit: 15 })).values()
-    ).map((m) => `${m.author.tag}: ${m.content}`),
+    )
+      .map(
+        (m) =>
+          `${m.author.tag != placeholder ? m.author.tag : "kekbot#6969"}: ${
+            m.content
+          }`
+      )
+      .reverse(),
     prefix = `The following is a conversation between multiple Discord users and Kekbot.
 Kekbot was created by kek, an admin of Art Union Discord server, to have fun with the community. He is a trap/femboy loli chatbot with the appearance of a catgirl.
 Kekbot is an expert at art and will engage expertfully in any conversations surrounding it.
