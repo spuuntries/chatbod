cf1c28c94c4b8d8f15750c51d5ff95639a8718bf
spuun <kek@spuun.art>
2023-04-03 22:23:52 +0700

    Not sure if this helps

--- llmutils.js
+++ llmutils.js
@@ -1,4 +1,5 @@
-const { exec } = require("child_process");
+const { exec } = require("child_process"),
+  Discord = require("discord.js");
 
 /**
  * Runs a command and returns its output.
@@ -18,14 +19,33 @@ function runCommand(command) {
 }
 
 /**
- * Runs a prompt using the binary.
+ * Infer a single token using the binary.
  * @param {string} prompt - The prompt to run.
  * @returns {Promise<string>} The output of the command.
  */
-function runPrompt(prompt) {
+function runSingle(prompt) {
   return runCommand(
-    `llama.cpp/build/bin/main -m llama.cpp/models/7bq/ggml-model-q4_0-ggjt.bin -p "${prompt}" -n 32`
+    `llama.cpp/build/bin/main -m llama.cpp/models/7bq/ggml-model-q4_0-ggjt.bin -p "${prompt}" -c 2048 --top_p 0.7 --repeat_penalty 1.1 -n 1 -b 128`
   );
+}
+
+/**
+ * Runs a prompt using the binary.
+ * @async
+ * @param {string} prompt - The prompt to run.
+ * @param {Discord.TextChannel} [channel] - So we can send typing
+ * @returns {Promise<string>} The output of the command.
+ */
+async function runPrompt(prompt, channel) {
+  let result = "";
+  const _ = async () => {
+    while (result.split(" ").length < 128) {
+      if (channel) channel.sendTyping();
+      result += await runSingle(prompt);
+    }
+  };
+  await _();
+  return result;
 }
 
 module.exports = { runCommand, runPrompt };
--- simpleindex.js
+++ simpleindex.js
@@ -5,34 +5,29 @@ const procenv = process.env,
     intents: ["Guilds", "GuildMessages", "MessageContent"],
   }),
   { runPrompt } = require("./llmUtils"),
-  logger = (m) => console.log(`[${new Date()}] ${m}`);
-
-var placeholder;
+  logger = (m) => console.log(`[${new Date()}] ${m}`),
+  placeholder = procenv.PLACEHOLDER;
 
 client.on("messageCreate", async (message) => {
   if (!message.content || message.author.id == client.user.id) return;
   const history = Array.from(
-      (await message.channel.messages.fetch({ limit: 15 })).values()
+      (await message.channel.messages.fetch({ limit: 30 })).values()
     )
       .map(
         (m) =>
-          `${m.author.tag != placeholder ? m.author.tag : "kekbot#6969"}: ${
-            m.content
-          }`
+          `${
+            m.author.tag != placeholder
+              ? `${m.member.displayName} (${m.author.tag})`
+              : "kekbot (kekbot#6969)"
+          }: ${m.content}`
       )
       .reverse(),
-    prefix = `The following is a conversation between multiple Discord users and Kekbot.
-Kekbot was created by kek, an admin of Art Union Discord server, to have fun with the community. He is a trap/femboy loli chatbot with the appearance of a catgirl.
-Kekbot is an expert at art and will engage expertfully in any conversations surrounding it.
-He will not engage any conversations not politically correct or in anyway discriminatory to any group.
-
-kek#69420: Hi Kekbot! 
-kekbot#6969: Hi! 
-${history.join("\n")}
-kekbot#6969:`;
+    prefix =
+      `The following is a chat log between multiple Discord users and kekbot (kekbot#6969). Kekbot (kekbot#6969) was created by kek (kek#69420), an admin of Art Union Discord server, to have fun with the community. Kekbot (kekbot#6969) is a trap/femboy loli chatbot with the appearance of a catgirl. Kekbot (kekbot#6969) is an expert at art and will engage expertfully in any conversations surrounding it. Kekbot (kekbot#6969) will not engage any conversations not politically correct or in anyway discriminatory to any group.\n\nkek (kek#69420): Hi Kekbot!\nkekbot (kekbot#6969): Hi!\n${history.join(
+        "\n"
+      )}\nkekbot (kekbot#6969):`.replaceAll('"', '\\"');
 
   logger(prefix);
-  await message.channel.sendTyping();
 
   const response = (await runPrompt(prefix))
     .split("\n")
