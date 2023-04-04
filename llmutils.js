1a9be7f3e697e318b1094e60ca5c330c6bfd2e01
spuun <kek@spuun.art>
2023-04-04 23:22:28 +0700

    whoops

--- llmutils.js
+++ llmutils.js
@@ -8,9 +8,9 @@ const { exec } = require("child_process"),
  */
 function runPrompt(prompt, message) {
   return new Promise(async (resolve, reject) => {
-    const runner = exec(`llama.cpp/build/bin/main`, [
-      ` -m models/7bq/ggml-model-q4_0-ggjt.bin -p "${prompt}" -c 2048 --top_p 0.7 --repeat_penalty 1.1 -n 128 -b 128`,
-    ]);
+    const runner = exec(
+      `llama.cpp/build/bin/main -m models/7bq/ggml-model-q4_0-ggjt.bin -p "${prompt}" -c 2048 --top_p 0.7 --repeat_penalty 1.1 -n 128 -b 128`
+    );
     var res = "";
 
     runner.stdout.on("data", async (data) => {
