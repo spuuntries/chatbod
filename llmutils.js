bfa5bbf8b841c34f99bc7720c1b44cd4c3765ea0
spuun <kek@spuun.art>
2023-04-04 21:42:29 +0700

    I have an idea

--- llmutils.js
+++ llmutils.js
@@ -19,38 +19,19 @@ function runCommand(command) {
 }
 
 /**
- * Infer a single token using the binary.
- * @param {string} prompt - The prompt to run.
- * @returns {Promise<string>} The output of the command.
- */
-function runSingle(prompt) {
-  return runCommand(
-    `llama.cpp/build/bin/main -m models/7bq/ggml-model-q4_0-ggjt.bin -p "${prompt}" -c 2048 --top_p 0.7 --repeat_penalty 1.1 -n 1 -b 128`
-  );
-}
-
-/**
  * Runs a prompt using the binary.
  * @async
  * @param {string} prompt - The prompt to run.
- * @param {Discord.TextChannel} [channel] - So we can send typing
- * @returns {Promise<string>} The output of the command.
+ * @param {Discord.Message} reply - To do token-by-token
+ * @returns
  */
-async function runPrompt(prompt, channel) {
-  let result = prompt;
-  const _ = async () => {
-    while (
-      !result
-        .split("\n")
-        [prompt.split("\n").length - 1].split(":")[1]
-        .endsWith("\n")
-    ) {
-      if (channel) await channel.sendTyping();
-      result = await runSingle(result);
-    }
-  };
-  await _();
-  return result;
+async function runPrompt(prompt, reply) {
+  const runner = exec(
+    `llama.cpp/build/bin/main -m models/7bq/ggml-model-q4_0-ggjt.bin -p "${prompt}" -c 2048 --top_p 0.7 --repeat_penalty 1.1 -n 128 -b 128`
+  );
+  runner.stdout.on("data", (data) => {
+    console.log(data);
+  });
 }
 
 module.exports = { runCommand, runPrompt };
