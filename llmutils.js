1df879b3ff5e133039f475920db702e78ec1edbb
spuun <kek@spuun.art>
2023-04-06 21:20:52 +0700

    Seems like exec is blocking the thread Offloading to other thread

--- llmutils.js
+++ llmutils.js
@@ -4,35 +4,37 @@ const { exec } = require("child_process"),
 /**
  * Runs a command and returns its output.
  * @param {string} command - The command to run.
- * @param {Discord.Message} message - To do typing.
  * @returns {Promise<string>} The output of the command.
  */
-function runCommand(command, message) {
+function runCommand(command) {
   return new Promise((resolve, reject) => {
-    const runner = exec(command, (err, stdout, stderr) => {
+    exec(command, (err, stdout, stderr) => {
       if (err) {
         reject(err);
       } else {
         resolve(stdout.trim());
       }
     });
-    runner.stdout.on("data", async () => {
-      await message.channel.sendTyping();
-    });
   });
 }
 
 /**
  * Runs a prompt using the binary.
+ * @async
  * @param {string} prompt - The prompt to run.
  * @param {Discord.Message} message - To display typing.
  * @returns {Promise<string>} The output of the command.
  */
-function runPrompt(prompt, message) {
-  return runCommand(
-    `llama.cpp/build/bin/main -m models/7bq/ggml-model-q4_0-ggjt.bin -p "${prompt}" -t 3 -c 2048 -n 128 -b 512`,
-    message
-  );
+async function runPrompt(prompt, message) {
+  const typing = setInterval(async () => {
+      await message.channel.sendTyping();
+    }, 10000),
+    res = await runCommand(
+      `llama.cpp/build/bin/main -m models/7bq/ggml-model-q4_0-ggjt.bin -p "${prompt}" -t 3 -c 2048 -n 128 -b 512`,
+      message
+    );
+  clearInterval(typing);
+  return res;
 }
 
 module.exports = { runCommand, runPrompt };
