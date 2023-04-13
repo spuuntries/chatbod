46b4f993040f127a7405de571a8cb8815cfdc2de
spuun <kek@spuun.art>
2023-04-13 21:40:12 +0700

    Lowering max tokens

--- infer.py
+++ infer.py
@@ -11,5 +11,5 @@ llm = Llama(
 
 
 def generate(prompt):
-    output = llm(prompt, max_tokens=64, stop=["\n"])
+    output = llm(prompt, max_tokens=42, stop=["\n"])
     return output["choices"][0]["text"]
