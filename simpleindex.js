6699e6324e829eac2241e71e6160b473483c8b45
spuun <kek@spuun.art>
2023-04-23 22:23:04 +0700

    Whoops

--- simpleindex.js
+++ simpleindex.js
@@ -118,7 +118,7 @@ client.on("messageCreate", async (message) => {
     response = responses
       .slice(
         splitResponses.at(-2).index,
-        splitResponses.at(-1).index ? splitResponses.at(-1) : undefined
+        splitResponses.at(-1).index ? splitResponses.at(-1).index : undefined
       )
       .split(":")[1];
 
