d9ad198af9ef7d9f74b11ae505fb24d544456e5b
spuun <kek@spuun.art>
2023-05-01 14:28:45 +0700

    Fix logging

--- simpleindex.js
+++ simpleindex.js
@@ -160,7 +160,7 @@ client.on("messageCreate", async (message) => {
     lastPrefix = responses.slice(prefix.length).search(/^[\w]+:/gim),
     response;
 
-  logger(responses);
+  logger(responses.slice(prefix.length));
 
   if (lastPrefix == -1) response = responses;
   else response = responses.slice(prefix.length).slice(0, lastPrefix);
