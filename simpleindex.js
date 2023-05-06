e5363614789273a98787ba5568d77a8e5c4f6e1e
spuun <kek@spuun.art>
2023-05-06 18:48:08 +0700

    Attempting to fix `\` escapes

--- simpleindex.js
+++ simpleindex.js
@@ -12,36 +12,8 @@ const procenv = process.env,
   logger = (m) => console.log(`[${new Date()}] ${m}`),
   placeholder = procenv.PLACEHOLDER;
 
-var llm,
-  responding = false,
+var responding = false,
   typing;
-
-/**
- * Finds the string between the specified start delimiter and the next prefix in the array.
- * @param {string[]} messages - The array of strings to search.
- * @param {string} startDelimiter - The start delimiter to search for.
- * @returns {string|null} The concatenated string or null if the start delimiter was not found.
- */
-function concatUntilNextPrefix(messages, startDelimiter) {
-  const startIndex = messages.indexOf(startDelimiter);
-
-  if (startIndex !== -1) {
-    let endIndex = messages.length;
-
-    // Find the index of the next prefix
-    for (let i = startIndex + 1; i < messages.length; i++) {
-      if (/^[\w-]+:/.test(messages[i])) {
-        endIndex = i;
-        break;
-      }
-    }
-
-    const inBetween = messages.slice(startIndex + 1, endIndex).join("");
-    return messages[startIndex] + inBetween;
-  } else {
-    return null;
-  }
-}
 
 /**
  * @param {string} str To extract from
@@ -119,47 +91,15 @@ client.on("messageCreate", async (message) => {
           }`
       )
       .reverse(),
-    prefix =
-      `The following is a chat log between multiple Discord users and Kekbot. Kekbot can respond with a GIF when indicated with the special keyword \\"[gif]\\". Emotes are indicated by colons, e.g. \\":pepega:\\", kekbot doesn't like using emotes. Image attachments are indicated by parentheses with their captions in them, e.g. \\"(an image of a gun on a table)\\". Kekbot was created by kek (@kkekkyea), an admin of Art Union Discord server, Kekbot is not kek. Kekbot was created to help and have fun with the community. Kekbot is a loli chatbot with the appearance of a catgirl. Kekbot is an expert in all forms of art will always try to help when asked to. Kekbot will never send an empty reply. Kekbot is friendly to everyone.\n\nRed: Hi Kekbot!\nkekbot: Enlo ther! [gif]\nBlue: How u doin? :thinking:\nkekbot: I'm gud, ty for asking!\nRed: Who are you?\nkekbot: Me am a smol chatbot made by kek!${
-        history.length ? "\n" + (await Promise.all(history)).join("\n") : ""
-      }\nkekbot:`.replaceAll(`"`, `\"`);
+    prefix = `The following is a chat log between multiple Discord users and Kekbot. Kekbot can respond with a GIF when indicated with the special keyword \\\"[gif]\\\". Emotes are indicated by colons, e.g. \\\":pepega:\\\", kekbot doesn't like using emotes. Image attachments are indicated by parentheses with their captions in them, e.g. \\\"(an image of a gun on a table)\\\". Kekbot was created by kek (@kkekkyea), an admin of Art Union Discord server, Kekbot is not kek. Kekbot was created to help and have fun with the community. Kekbot is a loli chatbot with the appearance of a catgirl. Kekbot is an expert in all forms of art will always try to help when asked to. Kekbot will never send an empty reply. Kekbot is friendly to everyone.\n\nRed: Hi Kekbot!\nkekbot: Enlo ther! [gif]\nBlue: How u doin? :thinking:\nkekbot: I'm gud, ty for asking!\nRed: Who are you?\nkekbot: Me am a smol chatbot made by kek!${
+      history.length
+        ? "\n" + (await Promise.all(history)).join("\n").replaceAll(`"`, `\\\"`)
+        : ""
+    }\nkekbot:`;
 
   logger(prefix);
 
-  // NOTE: Scrapped for the moment to see if native is faster.
-  //function type() {
-  //  message.channel.sendTyping().then(() => {
-  //    typing = setTimeout(() => {
-  //      type();
-  //    }, 15000);
-  //  });
-  //}
-  //type();
-  //
-  //var response = await llm.generate$(prefix, { $timeout: 125000 }),
-  //  index = response.search(/^[\w]+:/m); // Find the end of the response
-  //if (index >= 0)
-  //  response = response
-  //    .substring(0, index)
-  //    .replaceAll("<turn>", "")
-  //    .replace(/\n$/, ""); // Gets the response and removes newline if found at the end.
-  //
-  //clearTimeout(typing);
-
-  /** var responses = (await runPrompt(prefix, message)).split("<turn>"),
-    response = responses
-      .slice(responses.length - 1)
-      .filter((msg) => msg.toLowerCase().startsWith("\nkekbot:"))
-      .shift()
-      .split(":")[1];*/
-  /** 
-    response = concatUntilNextPrefix(
-      responses,
-      responses[prefix.split("\n").length - 1]
-    ).split(":")[1];
-    */
-
-  var responses = (await runPrompt(prefix)).replaceAll(`"`, `\"`),
+  var responses = (await runPrompt(prefix)).replaceAll(`"`, `\\\"`),
     lastPrefix = responses.slice(prefix.length).search(/^[^ ]+:/gim),
     response;
 
@@ -210,7 +150,6 @@ client.on("messageCreate", async (message) => {
 });
 
 client.on("ready", async () => {
-  //llm = await python("./infer-bindings.py");
   client.user.setPresence({
     status: "idle",
     activities: [
