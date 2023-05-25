aed43715fdea67a787191d93d0911bec5aa7c053
spuun <kek@spuun.art>
2023-05-25 13:27:38 +0700

    Forgot to change model

--- llmutils.js
+++ llmutils.js
@@ -150,7 +150,7 @@ async function generateImage(query) {
   const res = Buffer.from(
     await (
       await hf.textToImage({
-        model: "andite/pastel-mix",
+        model: "gsdf/Counterfeit-V2.5",
         inputs: `${
           keywords ? `${keywords},` : ""
         } ${emotion}, ${emotion}, ${emotion}, 1girl, green hair, loli, femboy, masterpiece, best quality, looking at viewer`,
