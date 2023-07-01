2f056ab424d3f2ccb1b2a22165b464456b89b0cb
spuuntries <kek@spuun.art>
2023-07-01 19:06:38 +0700

    Remove mlock, top p, and mmap param

--- infer-bindings.py
+++ infer-bindings.py
@@ -7,8 +7,6 @@ llm = Llama(
     n_ctx=2048,
     n_batch=512,
     n_threads=4,
-    use_mlock=True,
-    use_mmap=False,
 )
 cache = LlamaDiskCache(capacity_bytes=67108864)
 llm.set_cache(cache)
@@ -18,10 +16,9 @@ def generate(prompt):
     output = llm(
         prompt,
         max_tokens=48,
-        temperature=0.85,
+        temperature=0.9,
         mirostat_mode=2,
         repeat_penalty=1.08,
-        top_p=0.65,
     )
     if output["choices"][0]["finish_reason"] == "length":
         output["choices"][0]["text"] = output["choices"][0]["text"] + "—"
