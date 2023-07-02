3151bebdfbeb7486a9a5bf46cd391fc8890fc2ec
spuuntries <kek@spuun.art>
2023-07-02 14:16:39 +0700

    Bump batch size

--- infer-bindings.py
+++ infer-bindings.py
@@ -5,7 +5,7 @@ llm = Llama(
     model_path="./models/13bpq/pyg.bin",
     seed=random.randint(1, 9999),
     n_ctx=2048,
-    n_batch=512,
+    n_batch=1024,
     n_threads=4,
 )
 cache = LlamaDiskCache()
