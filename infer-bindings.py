17a9d1b32b305772f2e6f4beb15da3186dc91683
spuuntries <kek@spuun.art>
2023-10-14 23:54:47 +0700

    feat: decouple gen configs into separate file

--- infer-bindings.py
+++ infer-bindings.py
@@ -1,31 +1,28 @@
 from llama_cpp import Llama, LlamaDiskCache
 from dotenv import load_dotenv
 import random
+import tomllib
 import os
 
 load_dotenv()
+with open(os.environ["LLMCONFIG"], "rb") as f:
+    llm_config = tomllib.load(f)
 
 llm = Llama(
+    **llm_config["init"],
     model_path=os.environ["MODELPATH"],
     seed=random.randint(1, 9999),
-    n_ctx=4096,
-    n_batch=1024,
-    n_threads=4,
-    use_mlock=True,
-    use_mmap=False,
-    rope_freq_scale=0.5,
 )
 cache = LlamaDiskCache()
 llm.set_cache(cache)
 
 
 def generate(prompt):
+    with open(os.environ["LLMCONFIG"], "rb") as f:
+        llm_config = tomllib.load(f)
     output = llm(
+        **llm_config["gen"],
         prompt,
-        max_tokens=64,
-        temperature=0.7,
-        mirostat_mode=2,
-        repeat_penalty=1.08,
     )
     if output["choices"][0]["finish_reason"] == "length":
         output["choices"][0]["text"] = output["choices"][0]["text"] + "—"
