ac84be0fdc5522cc9ed04d5bb0580ae477775927
spuuntries <kek@spuun.art>
2023-12-24 20:48:54 +0700

    feat: Try openrouter inference

--- infer-paid.js
+++ infer-paid.js
@@ -2,29 +2,49 @@ const Replicate = require("replicate"),
   dotenv = require("dotenv");
 
 dotenv.config();
-const procenv = process.env,
-  replicate = new Replicate({
-    auth: procenv.REPTOKEN,
-  });
+const procenv = process.env;
+// replicate = new Replicate({
+//   auth: procenv.REPTOKEN,
+// });
 
 async function generate(prompt) {
   return (
-    await replicate.run(
-      "mistralai/mistral-7b-instruct-v0.2:f5701ad84de5715051cb99d550539719f8a7fbcf65e0e62a3d1eb3f94720764e",
-      {
-        input: {
+    await (
+      await fetch("https://openrouter.ai/api/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${procenv.ORTOKEN}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          model: "mistralai/mixtral-8x7b-instruct",
+          prompt: prompt,
+          max_tokens: 256,
           top_k: 50,
           top_p: 0.9,
-          prompt: prompt,
-          temperature: 0.6,
-          max_new_tokens: 256,
-          prompt_template: "<s>{prompt}",
-          presence_penalty: 0,
           frequency_penalty: 1.1,
-        },
-      }
-    )
-  ).join("");
+          temperature: 0.6,
+        }),
+      })
+    ).json()
+  ).choices[0].text;
+  // return (
+  //   await replicate.run(
+  //     "mistralai/mistral-7b-instruct-v0.2:f5701ad84de5715051cb99d550539719f8a7fbcf65e0e62a3d1eb3f94720764e",
+  //     {
+  //       input: {
+  //         top_k: 50,
+  //         top_p: 0.9,
+  //         prompt: prompt,
+  //         temperature: 0.6,
+  //         max_new_tokens: 256,
+  //         prompt_template: "<s>{prompt}",
+  //         presence_penalty: 0,
+  //         frequency_penalty: 1.1,
+  //       },
+  //     }
+  //   )
+  // ).join("");
 }
 
 async function generateImage(prompt, neg) {
