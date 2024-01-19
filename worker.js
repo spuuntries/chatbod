5bdcd0e4b3c04745f3a02cd925078ea1cec9dd5d
spuuntries <kek@spuun.art>
2024-01-19 17:08:40 +0700

    fix: Catch leading spaces in parser

--- worker.js
+++ worker.js
@@ -270,7 +270,7 @@ kekbot:`,
 
   /** @type {string} */
   var response = (await runPrompt(prefix)).replace("<END>", ""),
-    lastPrefix = response.search(/^[^ \n]+:/gim);
+    lastPrefix = response.search(/^ [^ \n]+:/gim);
 
   /**
    * @param {string} resp
@@ -299,7 +299,7 @@ kekbot:`,
           ],
         })
       ).replace("<END>", "");
-      lastPrefix = response.search(/^[^ \n]+:/gim);
+      lastPrefix = response.search(/^ [^ \n]+:/gim);
       if (lastPrefix >= 0) response = response.slice(0, lastPrefix);
       logger(
         `Caught repetition, regenerated with ${[
