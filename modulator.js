0bd6e0368d7832f4f6317f3c340e6bb07ae22a07
spuuntries <kek@spuun.art>
2024-02-19 22:21:56 +0700

    fix: Bump modulator weight

--- modulator.js
+++ modulator.js
@@ -14,7 +14,7 @@ function transform(str) {
       const matches = res.text().match(/(\w+)|(\s+)|([!-\/:-@[-`{-~])/gi);
       for (let i = 0; i < matches.length; i++) {
         if (matches[i].trim() !== "") {
-          if (_.inRange(_.random(1, 20), 1, 17)) {
+          if (_.inRange(_.random(1, 20), 1, 11)) {
             result += matches[i];
             continue;
           }
